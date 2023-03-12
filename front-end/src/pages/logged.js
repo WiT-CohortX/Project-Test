77ddab9f8e5f1520d493f43b4767e3684293b271
Carol <carolandrades11@gmail.com>
2023-03-12 23:58:17 +0000

    feat: create Logged page

---
+++ front-end/src/pages/logged.js
@@ -0,0 +1,9 @@
+import styles from "@/styles/Home.module.css";
+import Link from "next/link"
+
+export default function Logged() {
+  return <main>
+    Logged
+    <Link href="/">Logout</Link>
+    </main>;
+}
