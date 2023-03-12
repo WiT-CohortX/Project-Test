0fdf3fbcf11909fb86f95bf9d8c5ff711ee6b019
Carol <carolandrades11@gmail.com>
2023-03-12 23:56:02 +0000

    refactor: change structure of page

--- front-end/src/pages/_document.js
+++ front-end/src/pages/_document.js
@@ -3,8 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link href="/favicon.png" rel="icon" sizes="any" type="image/png" />
+      </Head>
+      <body className="p-4">
         <Main />
         <NextScript />
       </body>
